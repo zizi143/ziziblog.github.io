4c01f76ce2dd0a61bf049a8eeddc259dce240980
zizi143 <2950601037@qq.com>
2024-11-01 18:45:13 +0800

    Site updated: 2024-11-01 18:45:13

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/11/01/hello-world/"];function toRandomPost(){
+var posts=["2024/11/01/hello-world/","2024/11/01/【爱欲经济学】超性别化——美学享乐的自给自足，但并非自恋/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
